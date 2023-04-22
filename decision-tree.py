5f95949b0b7a8597b387b7714dd454db245f1529
horakhy <leonidas51000@hotmail.com>
2023-04-22 14:59:44 -0300

    feat: using pandas to read the data and new decisionTree

--- decision-tree.py
+++ decision-tree.py
@@ -1,25 +1,22 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor, plot_tree
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from load_the_data import given_data, expected_data
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from load_the_data import given_data, expected_data, expected_labels
 
+# print(expected_labels)
 
 ## Separar dataSet para treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(given_data, expected_data, test_size=0.3, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(given_data, expected_labels, test_size=0.7, random_state=0)
 
 ## Criar o modelo de árvore de decisão
-clf = DecisionTreeClassifier()
+dtree_model = DecisionTreeRegressor(max_depth=3)
+dtree_model.fit(X_train, y_train)
 
-## Treinar o modelo
-clf = clf.fit(X_train,y_train)
+## Predições
+dtree_predictions = dtree_model.predict(X_test)
 
-## Fazer previsões
-y_pred = clf.predict(X_test)
+print(np.sqrt(mean_squared_error(y_test, dtree_predictions)) * 100000)
 
-## Avaliar a acurácia
-# print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-# print("Precision:", metrics.precision_score(y_test, y_pred, average='weighted'))
-# print("Recall:", metrics.recall_score(y_test, y_pred, average='weighted'))
-# print("F1 score:", metrics.f1_score(y_test, y_pred, average='weighted'))
-
+plot_tree(dtree_model)
 
--- load_the_data.py
+++ load_the_data.py
@@ -1,8 +1,18 @@
 import csv
 
+import pandas as pd
+
 given_data = []
 expected_data = []
+classification_classes = {
+    1: "Crítico",
+    2: "Instável",
+    3: "Potencialmente estável",
+    4: "Estável" 
+}
 
+column_names_without_label = ["id", "pSist", "pDiast", "qPA", "Pulse", "fResp", "gravity"]
+column_names_with_label = ["id", "pSist", "pDiast", "qPA", "Pulse", "fResp", "gravity", "label"]
 
 with open('treino_sinais_vitais_sem_label.txt', newline='') as csvfile:
     # Create a reader object
@@ -15,14 +25,8 @@ with open('treino_sinais_vitais_sem_label.txt', newline='') as csvfile:
         row = [cell.strip() for cell in row]
         given_data.append(row)
 
-# read from com_label
-with open('treino_sinais_vitais_com_label.txt', newline='') as csvfile:
-    # Create a reader object
-    reader = csv.reader(csvfile, delimiter=',')
+given_data= pd.read_csv('treino_sinais_vitais_sem_label.txt', names=column_names_without_label, header=None)
 
-    # Initialize an empty list to store the data
+expected_data= pd.read_csv('treino_sinais_vitais_com_label.txt', names=column_names_with_label, header=None)
 
-    # Iterate over each row in the CSV file and append it to the data list
-    for row in reader:
-        row = [cell.strip() for cell in row]
-        expected_data.append(row)
+expected_labels = expected_data["label"]
