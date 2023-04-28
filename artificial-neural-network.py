a261fcefd889dbfe6cb2a07b2ef2be440f580b00
horakhy <leonidas51000@hotmail.com>
2023-04-28 18:04:43 -0300

    feat: artificial neural network classifier

--- artificial-neural-network.py
+++ artificial-neural-network.py
@@ -8,28 +8,35 @@ from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from sklearn.metrics import r2_score
-from load_the_data import given_data, evaluation_data, targeted_labels
+from load_the_data import given_data_classifier, targeted_labels
 
 target_column = ['label'] 
-predictors = list(set(list(given_data.columns))-set(target_column))
-given_data[predictors] = given_data[predictors]/given_data[predictors].max()
-given_data.describe().transpose()
 
-X = given_data[predictors].values
-y = given_data[target_column].values
+# X_attributes = given_data_classifier.drop(['label'], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(given_data, targeted_labels, test_size=0.7, random_state=0)
-print(X_train.shape); print(X_test.shape)
+predictors = list(set(list(given_data_classifier.columns))-set(target_column))
+max_value_predictors= given_data_classifier[predictors].max()
 
 
-mlp = MLPClassifier(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=5000)
+given_data_classifier[predictors] = given_data_classifier[predictors]/max_value_predictors ## Normalização dos dados
+given_data_classifier.describe().transpose()
+
+X = given_data_classifier[predictors].values
+y = given_data_classifier[target_column].values
+
+X_train, X_test, y_train, y_test = train_test_split(given_data_classifier, targeted_labels, test_size=0.3, random_state=1)
+print(X_train.shape)
+print(X_test.shape)
+
+
+mlp = MLPClassifier(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=100)
 mlp.fit(X_train,y_train)
 
 predict_train = mlp.predict(X_train)
 predict_test = mlp.predict(X_test)
 
 
-print(confusion_matrix(y_train,predict_train))
+print(confusion_matrix(y_train, predict_train))
 print(classification_report(y_train,predict_train))
 print(confusion_matrix(y_test,predict_test))
 print(classification_report(y_test,predict_test))
