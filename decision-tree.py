f9da06e15182ebfc3848313f7f22f3c650f65b30
horakhy <leonidas51000@hotmail.com>
2023-05-12 16:06:30 -0300

    New Classifier, without using gravity

--- decision-tree.py
+++ decision-tree.py
@@ -12,8 +12,13 @@ from load_the_data import (
     plot_confusion_matrix
 )
 import matplotlib.pyplot as plt
+import random
 
-def decision_tree_classifier():
+best_max_depth = 0
+best_random_state = 0
+best_accuracy_score = 0
+
+def decision_tree_classifier(max_depth):
     X_attributes = given_data_classifier.drop(["label"], axis=1)
 
     ## Separar dataSet para treinamento e teste
@@ -21,26 +26,31 @@ def decision_tree_classifier():
         X_attributes, targeted_labels, test_size=0.4, random_state=1
     )
     
-    dtree_model = DecisionTreeClassifier(criterion="gini", max_depth=3)
+    dtree_model = DecisionTreeClassifier(criterion="gini", max_depth=12)
     dtree_model.fit(X_train, y_train)
 
     ## Predições
     dtree_predictions = dtree_model.predict(X_test)
-    filename = "regressor-Tree.pickle"
-    pickle.dump(dtree_model, open(filename, "wb"))
+    # filename = "regressor-Tree.pickle"
+    # pickle.dump(dtree_model, open(filename, "wb"))
 
-    plot_tree(dtree_model)
-    plt.savefig("dtree-classifier.png")
+    # plot_tree(dtree_model)
+    # plt.savefig("dtree-classifier.png")
 
     print(classification_report(y_test, dtree_predictions))
-    plot_importance(dtree_model, "tree-classifier.png")
-    plot_confusion_matrix(dtree_model, X_test, y_test, "decision-tree-classifier")
+    # plot_importance(dtree_model, "tree-classifier.png")
+    # plot_confusion_matrix(dtree_model, X_test, y_test, "decision-tree-classifier")
+    
+    global best_accuracy_score, best_max_depth, best_random_state
+    if(accuracy_score(y_test, dtree_predictions) > best_accuracy_score):    
+        best_accuracy_score = accuracy_score(y_test, dtree_predictions)
+        best_max_depth = max_depth
 
-    export_graphviz(dtree_model, 
-      out_file='Tree.dot', 
-      feature_names = X_train.columns,
-      rounded = True, proportion = False, 
-      precision = 2, filled = True)
+    # export_graphviz(dtree_model, 
+    #   out_file='Tree.dot', 
+    #   feature_names = X_train.columns,
+    #   rounded = True, proportion = False, 
+    #   precision = 2, filled = True)
 
 def decision_tree_regressor():
     X_attributes = given_data_regressor.drop(["gravity"], axis=1)
@@ -66,5 +76,13 @@ def decision_tree_regressor():
     print(mean_squared_error(y_test, dtree_predictions))
 
 
-decision_tree_classifier()
+for i in range(1):
+    decision_tree_classifier(random.randint(1, 25))
+    print("Iteração: ", i)
+
 # decision_tree_regressor()
+
+
+# print("Best max_depth: ", best_max_depth)
+# print("Best random_state: ", best_random_state)
+# print("Best accuracy_score: ", best_accuracy_score)
--- load_the_data.py
+++ load_the_data.py
@@ -18,7 +18,7 @@ given_data_regressor= pd.read_csv('treino_sinais_vitais_com_label.txt', names=co
 given_data_classifier= pd.read_csv('treino_sinais_vitais_com_label.txt', names=column_names_with_label, header=None)
 
 given_data_regressor = given_data_regressor.drop(['id', 'pSist', 'pDiast', 'label'], axis=1)
-given_data_classifier = given_data_classifier.drop(['id', 'pSist', 'pDiast'], axis=1)
+given_data_classifier = given_data_classifier.drop(['id', 'pSist', 'pDiast', 'gravity'], axis=1)
 
 targeted_gravity = given_data_regressor["gravity"]
 targeted_labels = given_data_classifier["label"]
